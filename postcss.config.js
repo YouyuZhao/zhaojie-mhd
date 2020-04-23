aa45481e59be3cd229185bd22c28a286835eb0cf
YouyuZhao <1213366162@qq.com>
2020-04-23 19:22:58 +0800

    bugFix-browerslistrc

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,8 @@
 module.exports = {
     plugins: {
         autoprefixer: {
-            browsers: ['Android >= 4.0', 'iOS >= 8'],
+            //不需要配置，直接在.browserslistrc已经做好了设置
+            // browsers: ['Android >= 4.0', 'iOS >= 8'],
         },
         'postcss-pxtorem': {
             //转换的基准值 1rem=37.5px
